97c170021c1a64cd5d4540f642b9ac323d9f1c04
Chad Adams <chadeadams@gmail.com>
2017-12-05 14:34:43 -0600

    Updated build file structure

---
+++ build/windows-32/zip-32/req.bat
@@ -0,0 +1,3 @@
+@echo off
+cpan install Switch FTN::Packet
+echo Finished installing needed pre-reqs
--- build/zip/windows/req.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cpan install Switch FTN::Packet
-echo Finished installing needed pre-reqs
